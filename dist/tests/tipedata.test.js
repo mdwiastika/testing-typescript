25314d84da275885376f8c17e606b4986347e6de
Marcel Dwi Astika <marceldwias@gmail.com>
2023-07-12 20:04:23 +0700

    data type

---
+++ dist/tests/tipedata.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Data Type", () => {
+    it("Should Must Declare", () => {
+        let name = "Marcel Dwi Astika";
+        const isHuman = true;
+        const balance = 1000000000000;
+        name = "Marcel Update";
+        name = "Marcel Update ajalah";
+        name = "Marcel Update ajalah Oke";
+    });
+});
---
+++ tests/tipedata.test.ts
@@ -0,0 +1,10 @@
+describe("Data Type", () => {
+  it("Should Must Declare", () => {
+    let name: String = "Marcel Dwi Astika";
+    const isHuman: Boolean = true;
+    const balance: Number = 1000000000000;
+    name = "Marcel Update";
+    name = "Marcel Update ajalah";
+    name = "Marcel Update ajalah Oke";
+  });
+});
