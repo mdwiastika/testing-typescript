541d6e3066395a176f0621b0c1b6ec013faabfe6
Marcel Dwi Astika <marceldwias@gmail.com>
2023-07-12 20:46:45 +0700

    test any type

---
+++ dist/tests/any.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Testing Any Data Type", () => {
+    it("should be support any data type", () => {
+        const data = {
+            id: 1,
+            name: "Marcel",
+            isHuman: true,
+        };
+        console.info(data);
+    });
+});
---
+++ tests/any.test.ts
@@ -0,0 +1,10 @@
+describe("Testing Any Data Type", () => {
+  it("should be support any data type", () => {
+    const data: any = {
+      id: 1,
+      name: "Marcel",
+      isHuman: true,
+    };
+    console.info(data);
+  });
+});
