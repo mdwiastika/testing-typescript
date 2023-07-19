78b861464dd9f746cc3b32864275b69c0116e0e9
Marcel Dwi Astika <marceldwias@gmail.com>
2023-07-19 17:29:30 +0700

    test operator

--- tests/union.test.ts
+++ tests/union.test.ts
@@ -8,4 +8,18 @@ describe("Union Type", () => {
     console.info(sample);
     sample = [10, 20];
   });
+  it("should support typeof operator", () => {
+    function processData(value: number | string | boolean) {
+      if (typeof value === "string") {
+        return value.toUpperCase();
+      } else if (typeof value == "number") {
+        return value + 2;
+      } else {
+        return !value;
+      }
+    }
+    expect(processData("Marcel")).toBe("MARCEL");
+    expect(processData(10)).toBe(12);
+    expect(processData(true)).toBe(false);
+  });
 });
