3f900bfc6d17d8841a04f614ad96a74951e45dcd
Marcel Dwi Astika <marceldwias@gmail.com>
2023-07-21 22:11:05 +0700

    readonly property

--- src/seller.ts
+++ src/seller.ts
@@ -2,4 +2,6 @@ export interface Seller {
   id: number;
   name: string;
   address?: string;
+  readonly nip: string;
+  readonly npwp: string;
 }
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -5,7 +5,11 @@ describe("Interface Typescript", () => {
     const seller: Seller = {
       id: 2,
       name: "Budi",
+      nip: "912918212",
+      npwp: "929120120",
     };
+    seller.name = "Susanto";
+    // seller.nip = "018212"; //error karena readonly property
     console.info(seller);
   });
 });
